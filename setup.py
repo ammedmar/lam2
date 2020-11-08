e7ab1b62e39ed9916c1c8c54ad0d21e2f159e5fc
ammedmar <ammedmar@gmail.com>
2020-11-08 16:55:53 +0100

    Create package
    Signed-off-by: ammedmar <ammedmar@gmail.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lam2',
+      version='0.1',
+      description='Linear algebra with mod 2 coefficients',
+      url='https://github.com/ammedmar/lam2',
+      author='Anibal M. Medina-Mardones',
+      author_email='ammedmar@gmail.com',
+      license='MIT',
+      packages=['lam2'],
+      zip_safe=False)
